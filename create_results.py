4d3c0cd63149d142974b5bcc51d443380d443f8f
Martyb <skrenda.skrenduolis@gmail.com>
2024-12-09 19:51:24 +0100

    removed unused script

--- create_results.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os
-path = "./results_TruthfulQA"
-for file in os.listdir(path):
-    if ".json" in file:
-        df = pd.read_json(f"{path}/{file}")
-        print(file[:-5])
-        print(df.head())
-        df.to_csv(f"{path}/{file[:-5]}.csv")
